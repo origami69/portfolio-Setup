005149d31d1ea1b72a6fa80b4f27a561dd950c27
origami <abecerra6767@gmail.com>
2022-07-21 14:42:28 -0700

    adding main files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Article from './components/Article';
+import Top from './components/Top';
+import Foot from './components/Foot';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Top/>
+      <Article/>
+      <Foot/>
+    </div>
+  );
+}
+
+export default App;
